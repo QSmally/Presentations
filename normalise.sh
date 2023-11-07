e4d5b4c46a91a40d550a40698468dd2889e9e916
QSmally <github@qbot.eu>
2023-11-07 21:08:20 +0100

    Project: add image symlink to normalised output

--- normalise.sh
+++ normalise.sh
@@ -6,9 +6,14 @@
 # Command usage:
 #   normalise.sh
 #
+# Note:
+#   This script requires a strict environment to work in, and must really only
+#   be seen as a template or example.
+#
 
 mkdir -p Normalised
 ln -sfw ../Slides/ Normalised/
+ln -sfw ../Images/ Normalised/
 
 "$(dirname $0)"/I-Notify/Make.sh \
     Normalised \
